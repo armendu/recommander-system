85b45601e891b4a2579f905a0b04af145ce4e9a7
Armend Ukehaxhaj <armendd.u@hotmail.com>
2020-01-06 22:48:47 +0100

    Did minor refactoring

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ import numpy as np
 
 primary_data_filename = "input/Sample.txt"
 
+
 def count_words(filename):
     word_stats = {}
     with open(filename, "r", encoding="utf-8") as input:
@@ -26,7 +27,8 @@ def count_words(filename):
 
 
 def get_most_used_words(word_stats: dict, dict_size):
-    list_of_tuples = sorted(word_stats.items(), key= lambda kv:kv[1], reverse=True)
+    list_of_tuples = sorted(
+        word_stats.items(), key=lambda kv: kv[1], reverse=True)
     return [kv[0] for kv in list_of_tuples[:dict_size]]
 
 
@@ -46,28 +48,40 @@ def translate(filename, dict_size):
         for line in input.readlines():
             for word in line.split():
                 output.append(mapping.get(word, 0))
-    return output
+    return output, mapping
 
 
 def generate_batch(batch_size, skip_num, context_window):
-    global index 
+    global index
+    # TODO: Remove global value
+    index = 4
     assert skip_num <= context_window*2
     assert batch_size % skip_num == 0
     batch = np.ndarray(shape=(batch_size), dtype=np.int32)
     labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
     for i in range(batch_size//skip_num):
-        seen_words = [data[(index + k) % len(data)] for k in range(0 - context_window, context_window + 1)]
+        seen_words = [data[(index + k) % len(data)]
+                      for k in range(0 - context_window, context_window + 1)]
         given_word = seen_words.pop(context_window)
         for j in range(skip_num):
             batch[i*skip_num+j] = given_word
-            labels[i*skip_num+j, 0] = seen_words.pop(randint(0, len(seen_words) - 1))
+            labels[i*skip_num+j,
+                   0] = seen_words.pop(randint(0, len(seen_words) - 1))
         index = (index + 1) % len(data)
     return batch, labels
 
+
 def main():
     logger.info("Starting app")
-    global data = translate(primary_data_filename, 300)
-    logger.info(result)
+    global data
+    data, mapping = translate(primary_data_filename, 300)
+    # index = 4
+    batch, labels = generate_batch(8, 4, 2)
+    reverse_mapping = dict(zip(mapping.values(), mapping.keys()))
+
+    for i in range(8):
+        logger.info(str.format(
+            "{0}: {1}", reverse_mapping[batch[i]], reverse_mapping[labels[i][0]]))
 
 
 if __name__ == "__main__":
